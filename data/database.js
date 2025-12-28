3a097e7b9cba0b3dbe6caa9a559a45f892fac6a1
RamanS1819 <ramanspatel1819@gmail.com>
2025-12-28 21:59:52 +0530

    path updated

--- data/database.js
+++ data/database.js
@@ -5,8 +5,8 @@ export const connectDB = () => {
             .connect(process.env.MONGO_URI, {
                   dbName: "backendapi",
             })
-            .then(() => console.log(
-                  `Database Connected with ${process.env.MONGO_URI}`
+            .then((c) => console.log(
+                  `Database Connected with ${c.connection.host}`
             ))
             .catch((e) => console.log(e));
 };
